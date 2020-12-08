597ac0f35c0fd4ff6b1b6be24c6005ea684bf49a
kalipy <3069087972@qq.com>
2020-12-08 21:30:48 +0800

    实验2：根据bean的类型从IOC容器中获取bean的实例01

---
+++ src/test/java/com/ly/IOCTest.java
@@ -0,0 +1,22 @@
+package com.ly;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.ly.Person;
+
+public class IOCTest {
+
+    private ApplicationContext ioc = new ClassPathXmlApplicationContext("ioc.xml");
+
+    //实验2：根据bean的类型从IOC容器中获取bean的实例
+    //org.springframework.beans.factory.NoUniqueBeanDefinitionException
+    //No qualifying bean of type 'com.ly.Person' available: expected single matching bean but found 2: person01,person02
+    @Test 
+    public void test02() {
+        Person bean = ioc.getBean(Person.class);
+        System.out.println(bean);
+    }
+
+}
+
